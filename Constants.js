0701d1c69cb10551b36ac9b741cf7d1e3d48eea6
Skillabsc <161401031+Skillabsc@users.noreply.github.com>
2025-09-20 12:41:20 +0700

    Add files via upload

---
+++ Constants.js
@@ -0,0 +1,6 @@
+// File: lib/constants.js
+export const SLOT_CASTER_ABI = [
+    // Tambahkan ABI smart contract Anda di sini
+    // Contoh: { "constant": false, "inputs": [ ... ], "name": "spin" }
+];
+export const SLOT_CASTER_ADDRESS = "0x..."; // Ganti dengan alamat kontrak Anda
